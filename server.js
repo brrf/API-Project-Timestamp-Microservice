0f66ccf2092f69e3722b37fc4188082f35064eec
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-29 19:07:13 -0300

    created html to index

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/public/index.html')));
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));

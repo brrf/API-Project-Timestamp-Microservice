2a0f2863a6e7dd266b9cc91e409e1c6236ecf872
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-29 19:25:16 -0300

    added API route

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/public/index.html')));
 
+app.get('/api/timestamp/:date_string', (req, res) => res.json({ hello: "world!" }));
+
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));

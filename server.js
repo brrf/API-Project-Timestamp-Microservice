1b5d3d040a9851f9008c7c86a96bf9b28bf0c663
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-29 18:40:00 -0300

    Initial server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/index.html')));
+
+app.listen(PORT, () => console.log('Example app listening on port 3000!'));

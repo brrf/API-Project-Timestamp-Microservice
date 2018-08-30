100ad2e7611e5a86271322c074b1e3f56d921da4
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-29 21:19:03 -0300

    completed API datestring endpoint

--- server.js
+++ server.js
@@ -5,8 +5,29 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
+const parseDateString = (dateString) => {
+  const parsedNumber = Number(dateString);
+  const parsedDateString = new Date(dateString);
+  const tryToParseDataNumber = new Date(parsedNumber);
+  if (
+    (parsedDateString.toString() === "Invalid Date") &&
+    (tryToParseDataNumber.toString() === "Invalid Date")
+  ) {
+    return { error: "Invalid Date" };
+  } else if (parsedDateString.toString() !== "Invalid Date") {
+    return { unix: parsedDateString.getTime(), utc: parsedDateString.toUTCString() }; 
+  } else if (Number.isInteger(parsedNumber)) {
+    return { unix: tryToParseDataNumber.getTime(), utc: tryToParseDataNumber.toUTCString() };
+  }
+  return { error: "Invalid Date" };
+};
+
 app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/public/index.html')));
 
-app.get('/api/timestamp/:date_string', (req, res) => res.json({ hello: "world!" }));
+app.get('/api/timestamp/:date_string', (req, res) => {
+    const { date_string } = req.params // Descontrução de objetos, estude!
+    // Equivalente a const date_string = req.params.date_string
+    return res.json(parseDateString(date_string)) // Estude!!!!!!!
+});
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));

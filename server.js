7e5f28b26f01607b4235782a307b9e37100e0ce1
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-08-31 10:44:30 -0300

    Server modified

--- server.js
+++ server.js
@@ -11,14 +11,23 @@ const parseDateString = (dateString) => {
   const parsedNumber = Number(dateString);
   const parsedDateString = new Date(dateString);
   const tryToParseDataNumber = new Date(parsedNumber);
-  if (
+  var event = new Date(); 
+  if(
     (parsedDateString.toString() === "Invalid Date") &&
     (tryToParseDataNumber.toString() === "Invalid Date")
   ) {
+    if(isNaN(Number(dateString))){
+      return { unix: Date.now(), utc: event.toUTCString() };
+    } 
     return { error: "Invalid Date" };
-  } else if (parsedDateString.toString() !== "Invalid Date") {
+  }
+  else if (Number(dateString) === 0) { //Se não houver URL parameter
+    return { unix: Date.now(), utc: tryToParseDataNumber.toUTCString() };
+  }
+  else if (parsedDateString.toString() !== "Invalid Date") {
     return { unix: parsedDateString.getTime(), utc: parsedDateString.toUTCString() }; 
-  } else if (Number.isInteger(parsedNumber)) {
+  } 
+  else if (Number.isInteger(parsedNumber)) {
     return { unix: tryToParseDataNumber.getTime(), utc: tryToParseDataNumber.toUTCString() };
   }
   return { error: "Invalid Date" };
@@ -30,6 +39,13 @@ app.get('/api/timestamp/:date_string', (req, res) => {
     const { date_string } = req.params // Descontrução de objetos, estude!
     // Equivalente a const date_string = req.params.date_string
     return res.json(parseDateString(date_string)) // Estude!!!!!!!
+});
+
+app.get('/api/timestamp/', (req, res) => {
+  const { date_string } = req.params 
+  return res.json(parseDateString(date_string))
+  //Como ele não tem um URL parameter, deve-se pôr uma função que dê a data diretamente.
+  //Ou tentar criar uma exceção na função.
 });
 
 app.use(function(req, res) {
